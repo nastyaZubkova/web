5fa8c0324481fb28ebf3f28ab1c99469b3be3be9
Anastasiya Zubkova <nastya.zubkova.2021@mail.ru>
2023-05-10 22:48:19 +0300

    отдаю через разные функции, всё работает на главн

--- cmd/blog/handlers.go
+++ cmd/blog/handlers.go
@@ -28,18 +28,25 @@ type featuredPostData struct {
 }
 
 type mostResentPostData struct {
-	Title       string
-	Subtitle    string
-	Img         string
-	Author      string
-	AuthorImg   string
-	PublishDate string
+	Title       string `db:"title"`
+	Subtitle    string `db:"subtitle"`
+	Img         string `db:"image_url"`
+	Author      string `db:"author"`
+	AuthorImg   string `db:"author_url"`
+	PublishDate string `db:"publish_date"`
 }
 
 func index(db *sqlx.DB) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 
 		posts, err := featuredPosts(db)
+		if err != nil {
+			http.Error(w, "Internal Server Error", 500) // В случае ошибки парсинга - возвращаем 500
+			log.Println(err)
+			return // Не забываем завершить выполнение ф-ии
+		}
+
+		posts2, err := mostResentPosts(db)
 		if err != nil {
 			http.Error(w, "Internal Server Error", 500) // В случае ошибки парсинга - возвращаем 500
 			log.Println(err)
@@ -56,7 +63,7 @@ func index(db *sqlx.DB) func(w http.ResponseWriter, r *http.Request) {
 		data := indexPage{
 			Title:           "Escape",
 			FeaturedPosts:   posts,
-			MostResentPosts: mostResentPosts(),
+			MostResentPosts: posts2,
 		}
 
 		err = ts.Execute(w, data) // Заставляем шаблонизатор вывести шаблон в тело ответа
@@ -115,55 +122,26 @@ func featuredPosts(db *sqlx.DB) ([]featuredPostData, error) {
 	return posts, nil
 }
 
-func mostResentPosts() []mostResentPostData {
-	return []mostResentPostData{
-		{
-			Title:       "Still Standing Tall",
-			Subtitle:    "Life begins at the end of your comfort zone.",
-			Img:         "/static/images/still_tanding_tall.jpg",
-			Author:      "William Wong",
-			AuthorImg:   "/static/images/william_wong.jpg",
-			PublishDate: "9/25/2015",
-		},
-		{
-			Title:       "Sunny Side Up",
-			Subtitle:    "No place is ever as bad as they tell you it’s going to be.",
-			Img:         "/static/images/sunny_side_up.jpg",
-			Author:      "Mat Vogels",
-			AuthorImg:   "/static/images/mat_vogels.jpg",
-			PublishDate: "9/25/2015",
-		},
-		{
-			Title:       "Water Falls",
-			Subtitle:    "We travel not to escape life, but for life not to escape us.",
-			Img:         "/static/images/water_falls.jpg",
-			Author:      "Mat Vogels",
-			AuthorImg:   "/static/images/mat_vogels.jpg",
-			PublishDate: "9/25/2015",
-		},
-		{
-			Title:       "Through the Mist",
-			Subtitle:    "Travel makes you see what a tiny place you occupy in the world.",
-			Img:         "/static/images/throught_the_mist.jpg",
-			Author:      "William Wong",
-			AuthorImg:   "/static/images/william_wong.jpg",
-			PublishDate: "9/25/2015",
-		},
-		{
-			Title:       "Awaken Early",
-			Subtitle:    "Not all those who wander are lost.",
-			Img:         "/static/images/awaken_early.jpg",
-			Author:      "Mat Vogels",
-			AuthorImg:   "/static/images/mat_vogels.jpg",
-			PublishDate: "9/25/2015",
-		},
-		{
-			Title:       "Try it Always",
-			Subtitle:    "The world is a book, and those who do not travel read only one page.",
-			Img:         "/static/images/try_it_always.jpg",
-			Author:      "Mat Vogels",
-			AuthorImg:   "/static/images/mat_vogels.jpg",
-			PublishDate: "9/25/2015",
-		},
+func mostResentPosts(db *sqlx.DB) ([]mostResentPostData, error) {
+	const query = `
+		SELECT
+			title,
+			subtitle,
+			image_url,
+			author,
+			author_url,
+			publish_date
+		FROM
+			post
+		WHERE featured = 0
+	` // Составляем SQL-запрос для получения записей для секции most-resents-posts
+
+	var posts2 []mostResentPostData
+
+	err := db.Select(&posts2, query) // Делаем запрос в базу данных
+	if err != nil {                  // Проверяем, что запрос в базу данных не завершился с ошибкой
+		return nil, err
 	}
+
+	return posts2, nil
 }
